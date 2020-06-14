c87f40bbdcd7e5f70b524bd7f31a6a3b901ad4f3
Gabriel-Aragao <gabriellamparina@gmail.com>
2020-06-14 04:48:46 -0300

    Procfile added

--- src/app.ts
+++ src/app.ts
@@ -10,4 +10,9 @@ app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(3333);
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = '8000';
+}
+app.listen(port);
+
